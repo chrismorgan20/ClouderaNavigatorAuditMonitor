9b24dbaa0b28aeda007b7a76542efc63b102e279
Chris Morgan <chris@cjmorgan.org>
2018-06-23 20:20:26 -0700

    Add IP access failure monitoring and added functions to normalize usernames and IPs

--- ClouderaNavigatorAuditMonitor/eventMonitors.py
+++ ClouderaNavigatorAuditMonitor/eventMonitors.py
@@ -14,15 +14,61 @@
 
 import json
 
+def prepareUsername(user):
+    loc = user.find('@')
+    if loc > -1:
+        return (user[:loc])
+    else:
+        return user
+
+def prepareIP(ip):
+    if str(ip).startswith('/'):
+        return str(ip[1:])
+    else:
+        return str(ip)
+
 def runMonitors(events):
     monitorResults = {}
     c = getCounters(events)
     hueUsers = hueCreateServiceUser(events)
     sentry = getSentryActions(events)
+    ipMonitor = monitorIPSources(events)
     monitorResults['auth'] = c
     monitorResults['hue'] = hueUsers
     monitorResults['sentry'] = sentry
+    monitorResults['unknownFailedIPs'] = ipMonitor
+    print("Monitor Results")
+    print(json.dumps(monitorResults,indent=4))
     return monitorResults
+
+def monitorIPSources(events):
+    UnknownIPSourceFailure = {}
+    for host in events:
+        ipSourceSuccess = {}
+        ipSourceFailure = {}
+        for event in events[host]:
+            if 'username' in event.keys() and 'allowed' in event.keys() and 'ipAddress' in event.keys():
+                user = prepareUsername(event['username'])
+                ip = prepareIP(event['ipAddress'])
+                #add username key and initialize list to both IP Source dicts, if not there already
+                if user not in ipSourceSuccess.keys():
+                    ipSourceSuccess[user] = []
+                    ipSourceFailure[user] = []
+                if event['allowed'] and ip not in ipSourceSuccess[user]:
+                    ipSourceSuccess[user].append(ip)
+                elif not event['allowed'] and ip not in ipSourceFailure[user]:
+                    ipSourceFailure[user].append(ip)
+        UnknownIPSourceFailure[host] = {}
+        for foundUser in ipSourceSuccess.keys():
+            for failedIP in ipSourceFailure[foundUser]:
+                if failedIP not in ipSourceSuccess[foundUser]:
+                    if foundUser not in UnknownIPSourceFailure[host].keys():
+                        UnknownIPSourceFailure[host][foundUser] = []
+                        UnknownIPSourceFailure[host][foundUser].append(failedIP)
+                    else:
+                        UnknownIPSourceFailure[host][foundUser].append(failedIP)
+    return UnknownIPSourceFailure
+
 
 def getCounters(events):
     authDict = {}
@@ -37,23 +83,24 @@ def getCounters(events):
         for event in events[host]:
             if ('username' in event.keys()) and ('command' in event.keys()) and ('allowed' in event.keys()):
                 if event['command'] == 'authentication':
-                    key = event['username'] + event['ipAddress'] + str(event['allowed'])
+                    key = prepareUsername(str(event['username'])) + event['ipAddress'] + str(event['allowed'])
                     if key in authN:
                         authN[key]['count'] += 1
                     else:
                         authN[key] = {}
                         authN[key]['count'] = 1
-                        authN[key]['username'] = event['username'] 
+                        authN[key]['username'] = prepareUsername(str(event['username']))
                         authN[key]['command'] = event['command']
                         authN[key]['ipAddress'] = event['ipAddress']
                         authN[key]['allowed'] = str(event['allowed'])
                 elif ('service' in event.keys()):
-                    if (event['username'] in userFailures.keys()):
-                        userFailures[str(event['username'])].append(event)
+                    if (prepareUsername(str(event['username'])) in userFailures.keys()) and not event['allowed']:
+                        userFailures[prepareUsername(str(event['username']))].append(event)
                     else:
-                        userFailures[str(event['username'])] = []
-                        userFailures[str(event['username'])].append(event)
-                    key = event['username'] + event['ipAddress'] + event['service'] + str(event['allowed'])
+                        if not event['allowed']:
+                            userFailures[prepareUsername(str(event['username']))] = []
+                            userFailures[prepareUsername(str(event['username']))].append(event)
+                    key = prepareUsername(event['username']) + event['ipAddress'] + event['service'] + str(event['allowed'])
                     if key in authZ:
                         authZ[key]['count'] += 1
                     else:
@@ -65,7 +112,7 @@ def getCounters(events):
                         authZ[key]['allowed'] = str(event['allowed'])
             if ('username' in event.keys()) and ('command' not in event.keys()) and ('operationText' in event.keys()) and ('serviceValues' in event.keys()) and ('allowed' in event.keys()):
                 if str(event['operationText']).startswith("Successful login") and not event['serviceValues']:
-                    name = event['operationText'][16:]
+                    name = event['operationText'][(str(event['operationText']).find(':')+2):]
                     if name in cmAuthN['success'].keys():
                         cmAuthN['success'][name] = cmAuthN['success'][name] + 1
                     else:
@@ -76,8 +123,6 @@ def getCounters(events):
             'Authorization Failures':userFailures,
             'ClouderaManagerAuthN':cmAuthN
         }
-    print("AuthN/AuthZ Events")
-    print (json.dumps(authDict,indent=4))
     return authDict
 
 def hueCreateServiceUser(events):
@@ -89,8 +134,6 @@ def hueCreateServiceUser(events):
                 if ('hue' in str(event['service']).lower()) and (str(event['command']).lower() == 'create_user'):
                     createdUsers.append(event)
         hueCreates[host] = createdUsers
-    print("Users Created in HUE")
-    print(json.dumps(hueCreates,indent=4))
     return hueCreates
 
 def getSentryActions(events):
@@ -100,12 +143,12 @@ def getSentryActions(events):
         for event in events[host]:
             if ('service' in event.keys()):
                 if ('sentry' in str(event['service']).lower()):
+                    if str(event['ipAddress']).startswith('/'):
+                        event['ipAddress'] = str(event['ipAddress'])[1:]
                     if (event['username'] in sentryActions.keys()):
                         sentryActions[str(event['username'])].append(event)
                     else:
                         sentryActions[str(event['username'])] = []
                         sentryActions[str(event['username'])].append(event)
         sentryByHost[host] = sentryActions
-    print("Sentry Actions")
-    print(json.dumps(sentryByHost,indent=4))
     return sentryByHost
